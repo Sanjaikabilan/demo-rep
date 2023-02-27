a5d62452999e8019a0e0854d172e0ed35eeb96e9
Sanjaikabilan <83118883+Sanjaikabilan@users.noreply.github.com>
2023-02-27 08:24:22 +0000

    Static urls

--- projecto/graphe/urls.py
+++ projecto/graphe/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.team_name, name='team_name'),
@@ -7,3 +8,5 @@ urlpatterns = [
     path('chart', views.chart, name='chart'),
     path('sensor-data-json/<int:team_id>/', views.sensor_data_json, name='sensor_data_json'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
